86bee16df280a1f9166ddba3b2e0f2deaa7f2446
Dennis Prazak <prazak.dennis@gmail.com>
2020-12-02 11:25:53 +0100

    Day 2

---
+++ 2020/Solution/Day2.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using aoc2020;
+using FileParser;
+
+namespace Solution
+{
+    public class Day2 : IDay
+    {
+        public object Part1(IParsedFile file) {
+            var valid = 0;
+            while (!file.Empty) {
+                var line = file.NextLine();
+                var minmax = line.NextElement<string>().Split('-').Select(int.Parse).ToArray();
+                var ch = line.NextElement<string>()[0];
+                var str = line.NextElement<string>();
+                var cnt = str.Count(x => x == ch);
+                if (cnt >= minmax[0] && cnt <= minmax[1]) valid++;
+            }
+
+            return valid;
+        }
+
+        public object Part2(IParsedFile file) {
+            var valid = 0;
+            while (!file.Empty) {
+                var line = file.NextLine();
+                var rule = line.NextElement<string>().Split('-').Select(int.Parse).ToArray();
+                var ch = line.NextElement<string>()[0];
+                var str = line.NextElement<string>();
+                if (rule.Count(x => ch == str[x - 1]) == 1) valid++;
+            }
+
+            return valid;
+        }
+    }
+}
---
+++ 2020/Tests/Day2Test.cs
@@ -0,0 +1,29 @@
+﻿using FileParser;
+using Solution;
+using Xunit;
+
+namespace Tests
+{
+    public class Day2Test
+    {
+        private readonly IParsedFile fileMock;
+
+        public Day2Test() {
+            fileMock = new ParsedFile(new [] {
+                new ParsedLine("1-3 a: abcde".Split(' ')),
+                new ParsedLine("1-3 b: cdefg".Split(' ')),
+                new ParsedLine("2-9 c: ccccccccc".Split(' '))
+            });
+        }
+        [Fact]
+        public void Part1_Test() {
+            var day = new Day2();
+            Assert.Equal(2, day.Part1(fileMock));
+        }
+        [Fact]
+        public void Part2_Test() {
+            var day = new Day2();
+            Assert.Equal(1, day.Part2(fileMock));
+        }
+    }
+}
