c68f7fc3ae9816ab3559093f4ffb65777237c273
Dennis Prazak <prazak.dennis@gmail.com>
2023-12-11 09:27:46 +0100

    Day 11 cleanup

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -1,5 +1,5 @@
-use std::io::stdin;
 use itertools::Itertools;
+use std::io::{stdin, Read};
 
 type N = usize;
 
@@ -15,20 +15,28 @@ impl Position {
     }
 }
 
-const EXPANSION: N = 999999;
+fn silver(input: &str) -> N {
+    solve(input, 1)
+}
 
-fn main() {
+fn gold(input: &str) -> N {
+    solve(input, 999999)
+}
+
+fn solve(input: &str, expansion: N) -> N {
     let mut empty_rows = 0;
     let mut lines = vec![];
     let mut galaxy_map = vec![];
-    for (j, line) in stdin().lines().enumerate() {
-        let line = line.unwrap();
+    for (j, line) in input.split_terminator('\n').enumerate() {
         galaxy_map.push(vec![None; line.len()]);
         let mut empty = true;
         for (i, c) in line.chars().enumerate() {
             if c == '#' {
                 empty = false;
-                let galaxy = Position { x: i, y: j + empty_rows * EXPANSION };
+                let galaxy = Position {
+                    x: i,
+                    y: j + empty_rows * expansion,
+                };
                 galaxy_map[j][i] = Some(galaxy);
             }
         }
@@ -43,7 +51,7 @@ fn main() {
         for j in 0..galaxy_map.len() {
             if let Some(galaxy) = &mut galaxy_map[j][i] {
                 empty = false;
-                galaxy.x = i + empty_columns * EXPANSION;
+                galaxy.x = i + empty_columns * expansion;
             }
         }
         if empty {
@@ -52,6 +60,16 @@ fn main() {
     }
 
     let galaxies: Vec<Position> = galaxy_map.into_iter().flatten().flatten().collect();
-    let silver: N = galaxies.iter().combinations(2).map(|c| c[0].distance(c[1])).sum();
-    println!("silver: {}", silver);
+    galaxies
+        .iter()
+        .combinations(2)
+        .map(|c| c[0].distance(c[1]))
+        .sum()
+}
+
+fn main() {
+    let mut inp = String::new();
+    stdin().read_to_string(&mut inp).unwrap();
+    println!("silver: {}", silver(&inp));
+    println!("gold: {}", gold(&inp));
 }
