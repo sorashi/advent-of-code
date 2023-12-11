6443cc6340e85e6bba99fabe56e9f1bf6f0d207f
Dennis Prazak <prazak.dennis@gmail.com>
2023-12-11 09:23:01 +0100

    Day 11 gold

--- 2023/day11/src/main.rs
+++ 2023/day11/src/main.rs
@@ -1,12 +1,10 @@
-use std::cmp::Ordering;
 use std::io::stdin;
 use itertools::Itertools;
 
 type N = usize;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Position {
-    id: N,
     x: N,
     y: N,
 }
@@ -17,19 +15,21 @@ impl Position {
     }
 }
 
+const EXPANSION: N = 999999;
+
 fn main() {
-    let mut galaxies = vec![];
-    let mut id: N = 1;
     let mut empty_rows = 0;
     let mut lines = vec![];
+    let mut galaxy_map = vec![];
     for (j, line) in stdin().lines().enumerate() {
         let line = line.unwrap();
+        galaxy_map.push(vec![None; line.len()]);
         let mut empty = true;
         for (i, c) in line.chars().enumerate() {
             if c == '#' {
                 empty = false;
-                galaxies.push(Position { id, x: i, y: j + empty_rows });
-                id += 1;
+                let galaxy = Position { x: i, y: j + empty_rows * EXPANSION };
+                galaxy_map[j][i] = Some(galaxy);
             }
         }
         if empty {
@@ -38,35 +38,20 @@ fn main() {
         lines.push(line);
     }
     let mut empty_columns = 0;
-    for i in (0..lines[0].len()).rev() {
+    for i in 0..galaxy_map[0].len() {
         let mut empty = true;
-        for j in 0..lines.len() {
-            if lines[j].as_bytes()[i] == '#' as u8 {
+        for j in 0..galaxy_map.len() {
+            if let Some(galaxy) = &mut galaxy_map[j][i] {
                 empty = false;
-                break;
+                galaxy.x = i + empty_columns * EXPANSION;
             }
         }
         if empty {
             empty_columns += 1;
-            galaxies.iter_mut().filter(|g| g.x > i).for_each(|f| f.x += 1);
         }
     }
 
-    let width = galaxies.iter().map(|g| g.x).max().unwrap();
-    let height = galaxies.iter().map(|g| g.y).max().unwrap();
-    id = 0;
-    for j in 0..=height {
-        for i in 0..=width {
-            eprint!("{}", if galaxies.iter().any(|g| g.x == i && g.y == j) {
-                id += 1;
-                id.to_string()
-            } else { ".".to_string() });
-        }
-        eprintln!();
-    }
-    galaxies.iter().combinations(2).for_each(|c| {
-        eprintln!("galaxies {} and {} distance {}", c[0].id, c[1].id, c[0].distance(c[1]));
-    });
+    let galaxies: Vec<Position> = galaxy_map.into_iter().flatten().flatten().collect();
     let silver: N = galaxies.iter().combinations(2).map(|c| c[0].distance(c[1])).sum();
     println!("silver: {}", silver);
 }
