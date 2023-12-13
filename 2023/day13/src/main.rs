7f9db2e43cf68257ce6f41aad670d3d4c2915dd1
Dennis Prazak <prazak.dennis@gmail.com>
2023-12-13 10:23:20 +0100

    Day 13 gold

--- 2023/day13/src/main.rs
+++ 2023/day13/src/main.rs
@@ -6,32 +6,41 @@ fn check_for_reflections_vertically(
     width: usize,
     height: usize,
     accessor: fn(p: &[&[u8]], x: usize, y: usize) -> u8,
-) -> Vec<usize> {
-    let mut vertical_reflection_lines = vec![];
+) -> (Vec<usize>, Vec<usize>) {
+    let mut vertical_reflection_lines_silver = vec![];
+    let mut vertical_reflection_lines_gold = vec![];
     for possible_reflection_line in 0..width - 1 {
         let possible_reflection_width = min(
             width - possible_reflection_line - 1,
             possible_reflection_line + 1,
         );
-        let mut reflection = true;
-        'outer: for x in 0..possible_reflection_width {
+        let mut smudges = 0;
+        for x in 0..possible_reflection_width {
             for y in 0..height {
                 if accessor(pattern, possible_reflection_line - x, y)
                     != accessor(pattern, possible_reflection_line + x + 1, y)
                 {
-                    reflection = false;
-                    break 'outer;
+                    smudges += 1;
                 }
             }
         }
-        if reflection && possible_reflection_width > 0 {
-            vertical_reflection_lines.push(possible_reflection_line);
+        if possible_reflection_width > 0 {
+            if smudges == 0 {
+                vertical_reflection_lines_silver.push(possible_reflection_line);
+            } else if smudges == 1 {
+                vertical_reflection_lines_gold.push(possible_reflection_line);
+            }
         }
     }
-    vertical_reflection_lines
+    (
+        vertical_reflection_lines_silver,
+        vertical_reflection_lines_gold,
+    )
 }
 
-fn get_pattern_reflections(pattern: &[&[u8]]) -> (Vec<usize>, Vec<usize>) {
+fn get_pattern_reflections(
+    pattern: &[&[u8]],
+) -> ((Vec<usize>, Vec<usize>), (Vec<usize>, Vec<usize>)) {
     let width = pattern[0].len();
     let height = pattern.len();
     let vertical_reflection_lines =
@@ -46,6 +55,7 @@ fn main() {
     stdin().read_to_string(&mut input).unwrap();
     let patterns: Vec<_> = input.split_terminator("\n\n").collect();
     let mut silver = 0usize;
+    let mut gold = 0usize;
     for pattern in patterns {
         let pattern: Vec<_> = pattern
             .split_terminator('\n')
@@ -53,30 +63,16 @@ fn main() {
             .collect();
         let (vertical, horizontal) = get_pattern_reflections(&pattern);
 
-        eprint!("  ");
-        for x in 0..pattern[0].len() {
-            if vertical.contains(&x) {
-                eprint!("|");
-            } else {
-                eprint!(" ");
-            }
-        }
-        eprintln!();
-        for y in 0..pattern.len() {
-            if horizontal.contains(&y) {
-                eprint!("- ");
-            } else {
-                eprint!("  ");
-            }
-            for x in 0..pattern[0].len() {
-                eprint!("{}", pattern[y][x] as char);
-            }
-            eprintln!();
-        }
-
-        eprintln!("{:?} {:?}", vertical, horizontal);
-        silver += vertical.iter().map(|x| x + 1).sum::<usize>();
-        silver += horizontal.iter().map(|x| 100 * (x + 1)).sum::<usize>();
+        let (vertical_silver, vertical_gold) = vertical;
+        let (horizontal_silver, horizontal_gold) = horizontal;
+        silver += vertical_silver.iter().map(|x| x + 1).sum::<usize>();
+        silver += horizontal_silver
+            .iter()
+            .map(|x| 100 * (x + 1))
+            .sum::<usize>();
+        gold += vertical_gold.iter().map(|x| x + 1).sum::<usize>();
+        gold += horizontal_gold.iter().map(|x| 100 * (x + 1)).sum::<usize>();
     }
     println!("silver: {}", silver);
+    println!("gold: {}", gold);
 }
