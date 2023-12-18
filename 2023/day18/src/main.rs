c141755de54d78fbebba37eed99e101e2dae58ea
Dennis Prazak <prazak.dennis@gmail.com>
2023-12-18 11:01:58 +0100

    Day 18 cleanup

--- 2023/day18/src/main.rs
+++ 2023/day18/src/main.rs
@@ -1,47 +1,12 @@
-use aoc_utils::{TwoDimArray, Vector};
-use regex::Regex;
+use aoc_utils::Vector;
 use std::io::stdin;
 
 struct Direction {
     dir: Vector,
     len: isize,
-    color: String,
 }
 
-fn main() {
-    let mut terrain: TwoDimArray<u8> = TwoDimArray::new(1, 1);
-    let re = Regex::new(r"^([LRDU]) (\d+) \(#(.*?)\)$").unwrap();
-    let mut dirs = vec![];
-    for line in stdin().lines() {
-        let line = line.unwrap();
-        let caps = re.captures(&line).unwrap();
-        let dir = &caps[1];
-        let len: isize = (caps[2].parse::<isize>().unwrap());
-        let color = &caps[3];
-
-        let correct_len = isize::from_str_radix(&color[..5], 16).unwrap();
-        let correct_dir = match &color[5..] {
-            "0" => Vector::RIGHT,
-            "1" => Vector::DOWN,
-            "2" => Vector::LEFT,
-            "3" => Vector::UP,
-            _ => panic!()
-        };
-
-        let dir = match dir {
-            "L" => Vector::LEFT,
-            "R" => Vector::RIGHT,
-            "U" => Vector::UP,
-            "D" => Vector::DOWN,
-            _ => panic!(),
-        };
-        dirs.push(Direction {
-            dir: correct_dir,
-            len: correct_len,
-            color: color.to_string(),
-        });
-    }
-
+fn get_volume(dirs: &[Direction]) -> isize {
     let mut points = vec![];
     let mut point = dirs[0].dir * (dirs[0].len + 1);
     points.push(Vector::ZERO);
@@ -56,19 +21,19 @@ fn main() {
         let d1 = i.dir.x * j.dir.y - j.dir.x * i.dir.y;
         let d2 = k.dir.x * l.dir.y - l.dir.x * k.dir.y;
         if d1 > 0 && d2 > 0 {
-            // tato pravotočivá, další pravotočivá
+            // window1 is clockwise, window2 is clockwise angle
             point += j.dir * (j.len + 1);
         }
         if d1 > 0 && d2 <= 0 {
-            // tato pravotočivá, další levotočivá
+            // window1 is clockwise, window2 is counter-clockwise
             point += j.dir * j.len;
         }
         if d1 <= 0 && d2 <= 0 {
-            // tato levotočivá, další levotočivá
+            // window1 is counter-clockwise, window2 is counter-clockwise
             point += j.dir * (j.len - 1);
         }
         if d1 <= 0 && d2 >= 0 {
-            // tato levotočivá, další pravotočivá
+            // window1 is counter-clockwise, window2 is clockwise
             point += j.dir * j.len;
         }
 
@@ -82,6 +47,46 @@ fn main() {
         let p2 = window[1];
         running_volume += (p1.y + p2.y) * (p1.x - p2.x);
     }
+    running_volume / 2
+}
 
-    println!("{}", running_volume / 2);
+fn main() {
+    let mut silver_dirs = vec![];
+    let mut gold_dirs = vec![];
+    for line in stdin().lines() {
+        let line = line.unwrap();
+        let split: Vec<_> = line.split(' ').collect();
+        let silver_dir = split[0];
+        let silver_len: isize = split[1].parse().unwrap();
+        let color = &split[2][2..8];
+
+        let silver_dir = match silver_dir {
+            "L" => Vector::LEFT,
+            "R" => Vector::RIGHT,
+            "U" => Vector::UP,
+            "D" => Vector::DOWN,
+            _ => panic!(),
+        };
+
+        let gold_len = isize::from_str_radix(&color[..5], 16).unwrap();
+        let gold_dir = match &color[5..] {
+            "0" => Vector::RIGHT,
+            "1" => Vector::DOWN,
+            "2" => Vector::LEFT,
+            "3" => Vector::UP,
+            _ => panic!(),
+        };
+
+        silver_dirs.push(Direction {
+            dir: silver_dir,
+            len: silver_len,
+        });
+        gold_dirs.push(Direction {
+            dir: gold_dir,
+            len: gold_len,
+        });
+    }
+
+    println!("silver: {}", get_volume(&silver_dirs));
+    println!("gold: {}", get_volume(&gold_dirs));
 }
