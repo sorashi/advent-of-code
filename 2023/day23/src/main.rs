16448152e04cc4cf672ea49ab95d5c168cf5306c
Dennis Prazak <prazak.dennis@gmail.com>
2023-12-23 16:22:40 +0100

    Day 23 gold

--- 2023/day23/src/main.rs
+++ 2023/day23/src/main.rs
@@ -14,17 +14,63 @@ fn get_max_path(
     graph: &Graph,
     visited: &mut HashSet<Vector>,
     target: Vector,
-) -> usize {
+) -> isize {
     if point == target {
         return 0;
     }
-    let mut m = 0;
+    let mut m = isize::MIN;
 
     visited.insert(point);
     for (npos, n) in &graph[&point] {
-        m = m.max(get_max_path(*npos, graph, visited, target) + n)
+        if !visited.contains(npos) {
+            m = m.max(get_max_path(*npos, graph, visited, target) + *n as isize)
+        }
     }
+    visited.remove(&point);
     m
+}
+
+fn get_dirs_silver(c: u8) -> &'static [Vector] {
+    match c {
+        b'.' => &DIRECTIONS,
+        b'<' => &[Vector::LEFT],
+        b'>' => &[Vector::RIGHT],
+        b'^' => &[Vector::UP],
+        b'v' => &[Vector::DOWN],
+        _ => panic!(),
+    }
+}
+fn get_dirs_gold(c: u8) -> &'static [Vector] {
+    match c {
+        b'.' | b'>' | b'<' | b'^' | b'v' => &DIRECTIONS,
+        _ => panic!(),
+    }
+}
+
+fn build_graph(pois: &[Vector], map: &[&[u8]], get_dirs: fn(u8) -> &'static [Vector]) -> Graph {
+    let mut graph: Graph = pois.iter().map(|poi| (*poi, HashMap::new())).collect();
+    for poi in pois {
+        let mut stack = vec![(0, *poi)];
+        let mut visited = HashSet::new();
+        visited.insert(*poi);
+        while let Some((n, current)) = stack.pop() {
+            if n != 0 && pois.contains(&current) {
+                graph.get_mut(poi).unwrap().insert(current, n);
+                continue;
+            }
+            let dirs: &[Vector] = get_dirs(*current.get(map).unwrap());
+            for dir in dirs {
+                let pos = current + *dir;
+                if let Some(c) = pos.get(map) {
+                    if *c != b'#' && !visited.contains(&pos) {
+                        stack.push((n + 1, pos));
+                        visited.insert(pos);
+                    }
+                }
+            }
+        }
+    }
+    graph
 }
 
 fn main() {
@@ -65,38 +111,15 @@ fn main() {
     pois.push(start);
     pois.push(target);
 
-    let mut graph: Graph = pois.iter().map(|poi| (*poi, HashMap::new())).collect();
-    for poi in &pois {
-        let mut stack = vec![(0, *poi)];
-        let mut visited = HashSet::new();
-        visited.insert(*poi);
-        while let Some((n, current)) = stack.pop() {
-            if n != 0 && pois.contains(&current) {
-                graph.get_mut(poi).unwrap().insert(current, n);
-                continue;
-            }
-            let dirs: &[Vector] = match current.get(&map).unwrap() {
-                b'.' => &DIRECTIONS,
-                b'>' => &[Vector::RIGHT],
-                b'<' => &[Vector::LEFT],
-                b'^' => &[Vector::UP],
-                b'v' => &[Vector::DOWN],
-                _ => panic!(),
-            };
-            for dir in dirs {
-                let pos = current + *dir;
-                if let Some(c) = pos.get(&map) {
-                    if *c != b'#' && !visited.contains(&pos) {
-                        stack.push((n + 1, pos));
-                        visited.insert(pos);
-                    }
-                }
-            }
-        }
-    }
-
+    let graph = build_graph(&pois, &map, get_dirs_silver);
     println!(
         "silver: {}",
+        get_max_path(start, &graph, &mut HashSet::new(), target)
+    );
+
+    let graph = build_graph(&pois, &map, get_dirs_gold);
+    println!(
+        "gold: {}",
         get_max_path(start, &graph, &mut HashSet::new(), target)
     );
 }
