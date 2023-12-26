ff9953b4138e28cc47e8be1c7bb77e16d4d6099a
Dennis Prazak <prazak.dennis@gmail.com>
2023-12-26 13:49:41 +0100

    Day 25 clean up Python solution

--- 2023/day25-py/main.py
+++ 2023/day25-py/main.py
@@ -6,12 +6,11 @@ G = nx.Graph()
 for line in sys.stdin:
     line = line.strip()
     node, edges = line.split(':')
-
     for edge in [x.strip() for x in edges.strip().split(' ')]:
         G.add_edge(node, edge)
 
-cut = nx.minimum_edge_cut(G)
-G.remove_edges_from(cut)
-c = [len(c) for c in nx.connected_components(G)]
-print(f"silver: {c[0] * c[1]}")
+G.remove_edges_from(nx.minimum_edge_cut(G))
+l, r = [len(c) for c in nx.connected_components(G)]
+print(f"silver: {l * r}")
+print(f"gold: {l * r}")
 
