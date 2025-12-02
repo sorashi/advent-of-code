1c01422ee37e51ba8ea3f15c7ab5c499d559730f
Dennis Pražák <prazak.dennis@gmail.com>
2025-12-02 09:37:50 +0100

    Day 2 silver refactor

--- 2025/day02/src/main.rs
+++ 2025/day02/src/main.rs
@@ -8,24 +8,34 @@ fn main() {
     let mut silver = 0;
     for id_pair in inp.trim().split(',') {
         let (start, end) = id_pair.split_once('-').unwrap();
-        let mut current = start.split_at(start.len() / 2).0.parse::<N>().unwrap_or(1);
-        let start = start.parse::<N>().unwrap();
-        let end = end.parse::<N>().unwrap();
-        while repeat_num(current) < start {
-            current += 1;
-        }
-        while repeat_num(current) <= end {
-            silver += repeat_num(current);
-            current += 1;
-        }
+        silver += silver_one(start, end);
     }
     println!("silver: {silver}");
 }
 
-fn repeat_num(num: N) -> N {
+fn silver_one(start: &str, end: &str) -> N {
+    let mut silver = 0;
+    let mut current = start.split_at(start.len() / 2).0.parse::<N>().unwrap_or(1);
+    let start = start.parse::<N>().unwrap();
+    let end = end.parse::<N>().unwrap();
+    while repeat_num(current, 2) < start {
+        current += 1;
+    }
+    while repeat_num(current, 2) <= end {
+        silver += repeat_num(current, 2);
+        current += 1;
+    }
+    silver
+}
+
+fn repeat_num(num: N, times: u32) -> N {
     let digits = num.ilog10() as N;
     let hundreds = 10u128.pow(digits as u32 + 1);
-    num * hundreds + num
+    let mut result = num;
+    for _ in 1..times {
+        result = result * hundreds + num;
+    }
+    result
 }
 
 #[cfg(test)]
@@ -33,9 +43,13 @@ mod tests {
     use super::*;
     #[test]
     fn test_repeat_num() {
-        assert_eq!(repeat_num(12), 1212);
-        assert_eq!(repeat_num(5), 55);
-        assert_eq!(repeat_num(123), 123123);
-        assert_eq!(repeat_num(10), 1010);
+        assert_eq!(repeat_num(12, 2), 1212);
+        assert_eq!(repeat_num(5, 2), 55);
+        assert_eq!(repeat_num(123, 2), 123123);
+        assert_eq!(repeat_num(10, 2), 1010);
+        assert_eq!(repeat_num(1, 7), 1111111);
+        assert_eq!(repeat_num(123, 3), 123123123);
+        assert_eq!(repeat_num(12, 5), 1212121212);
+        assert_eq!(repeat_num(12, 1), 12);
     }
 }
