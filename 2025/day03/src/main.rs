1e7278ad2dd80414aa270dd056dd6d7b9534fe88
Dennis Pražák <prazak.dennis@gmail.com>
2025-12-03 11:58:47 +0100

    Day 3 gold

--- 2025/day03/src/main.rs
+++ 2025/day03/src/main.rs
@@ -1,22 +1,40 @@
 use std::io::stdin;
 
+type N = u64;
+fn get_joltage(mut bts: &[u8], mut count: usize) -> N {
+    let mut result = 0;
+    while count > 0 {
+        let (idx, v) = bts[0..bts.len() + 1 - count]
+            .iter()
+            .enumerate()
+            .min_by_key(|(_, val)| -(**val as i8))
+            .unwrap();
+        assert!(v.is_ascii_digit());
+        result = 10 * result + (v - b'0') as N;
+        bts = &bts[idx + 1..];
+        count -= 1;
+    }
+    result
+}
 fn main() {
     let mut silver = 0;
+    let mut gold = 0;
     for line in stdin().lines() {
         let line = line.unwrap();
         let bts = line.as_bytes();
-        let (i, v) = bts[..bts.len() - 1]
-            .iter()
-            .enumerate()
-            .min_by_key(|(_, val)| -(**val as i32))
-            .unwrap();
-        let v = *v;
-        let v2 = bts[i + 1..].iter().max().unwrap();
-        let v2 = *v2;
-        assert!(v.is_ascii_digit());
-        assert!(v2.is_ascii_digit());
-        let res = (v - b'0') as u32 * 10 + (v2 - b'0') as u32;
-        silver += res;
+        silver += get_joltage(bts, 2);
+        gold += get_joltage(bts, 12);
     }
     println!("silver: {}", silver);
+    println!("gold: {}", gold);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_get_joltage() {
+        assert_eq!(get_joltage(b"119198", 2), 99);
+        assert_eq!(get_joltage(b"119119", 3), 919);
+    }
 }
