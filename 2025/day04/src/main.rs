e509b766dcfd7f6095c3e82af09848a1ffbeae44
Dennis Pražák <prazak.dennis@gmail.com>
2025-12-04 09:31:19 +0100

    Day 4 gold

--- 2025/day04/src/main.rs
+++ 2025/day04/src/main.rs
@@ -1,17 +1,37 @@
-use std::io::{Read, stdin};
+use std::io::stdin;
 
 use aoc_utils::{TwoDimArray, Vector};
 
 fn main() {
     let mut arr = vec![];
     let mut silver = 0;
+    let mut gold = 0;
     for line in stdin().lines() {
         let line = line.unwrap();
         let mut row = vec![];
         row.extend_from_slice(line.as_bytes());
         arr.push(row);
     }
-    let arr: TwoDimArray<u8> = arr.into();
+    let mut arr: TwoDimArray<u8> = arr.into();
+    let mut removable = find_removable_rolls(&arr);
+    silver = removable.len();
+    gold += removable.len();
+    while !removable.is_empty() {
+        remove_removable_rolls(&mut arr, &removable);
+        removable = find_removable_rolls(&arr);
+        gold += removable.len();
+    }
+
+    println!("silver: {silver}");
+    println!("gold: {gold}");
+}
+fn remove_removable_rolls(arr: &mut TwoDimArray<u8>, removable: &[Vector]) {
+    for pos in removable {
+        debug_assert!(arr.get_by_vector(pos).unwrap() == &b'@');
+        arr.set(pos.x as usize, pos.y as usize, b'.').unwrap();
+    }
+}
+fn find_removable_rolls(arr: &TwoDimArray<u8>) -> Vec<Vector> {
     let dirs = [
         Vector::LEFT,
         Vector::RIGHT,
@@ -22,6 +42,7 @@ fn main() {
         Vector::SW,
         Vector::SE,
     ];
+    let mut res = vec![];
     for y in 0..arr.height() {
         for x in 0..arr.width() {
             let pos = Vector {
@@ -40,9 +61,9 @@ fn main() {
                 })
                 .count();
             if cnt < 4 {
-                silver += 1;
+                res.push(pos);
             }
         }
     }
-    println!("silver: {silver}");
+    res
 }
