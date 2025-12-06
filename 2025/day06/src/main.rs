e41bc815cbe96c82506b733bd568e9d6414d0cce
Dennis Pražák <prazak.dennis@gmail.com>
2025-12-06 08:53:48 +0100

    Day 6 gold

--- 2025/day06/src/main.rs
+++ 2025/day06/src/main.rs
@@ -1,5 +1,46 @@
 use std::io::stdin;
 
+fn gold(lines: &[String]) -> u64 {
+    let ops = lines.last().unwrap().as_bytes();
+    let mut result = 0;
+    let mut start = 0;
+    while start < ops.len() {
+        let mut i = start + 1;
+        while i < ops.len() && ops[i] == b' ' {
+            i += 1;
+        }
+        let range = start..i;
+        let op = ops[range.start];
+
+        let mut local_result = match op {
+            b'+' => 0,
+            b'*' => 1,
+            _ => unreachable!(),
+        };
+        for i in range {
+            let mut num = 0;
+            for j in 0..lines.len() - 1 {
+                let b = lines[j].as_bytes()[i];
+                if b == b' ' {
+                    continue;
+                }
+                num = 10 * num + (b - b'0') as u64;
+            }
+            if num == 0 {
+                continue;
+            }
+            local_result = match op {
+                b'+' => local_result + num,
+                b'*' => local_result * num,
+                _ => unreachable!(),
+            };
+        }
+        start = i;
+        result += local_result;
+    }
+    result
+}
+
 fn main() {
     let lines = stdin().lines().map(|l| l.unwrap()).collect::<Vec<_>>();
     let mut hw = vec![];
@@ -25,4 +66,5 @@ fn main() {
         silver += res;
     }
     println!("silver: {}", silver);
+    println!("gold: {}", gold(&lines));
 }
